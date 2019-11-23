8889960c98e6d4c5eed144b14c0037e49b97917b
Blessing Alfred <blealf@gmail.com>
2019-11-22 20:47:23 -0500

    code commit

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/svg/insta.jpg" class="rounded-circle"></img:src>
+        </div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="http://"> Add New Post</a>          
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>241</strong> posts</div>
+                <div class="pr-5"><strong>34.1k</strong> followers</div>
+                <div class="pr-5"><strong>233</strong> following</div>
+            </div>
+            <div>
+                <div class="pt-4 font-weight-bold">{{ $user->profile->title ?? 'Nothing yet'}}</div>
+                <div>{{ $user->profile->description ?? 'Nothing yet'}}</div>
+                <div><a href="#">{{ $user->profile->url ?? 'coming soon...'}}</a></div>
+            </div>
+        </div>
+
+        <div class="row pt-5">
+            <div class="col-4">
+                <img src="/svg/profile.png" class="w-100">           
+            </div>
+            <div class="col-4">
+                <img src="/svg/profile.png" class="w-100">           
+            </div>
+            <div class="col-4">
+                <img src="/svg/profile.png" class="w-100">           
+            </div>
+        </div>
+        
+    </div>
+</div>
+@endsection
